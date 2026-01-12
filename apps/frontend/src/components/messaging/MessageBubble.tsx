b880b175010f9029445f48f29272566deccbf645
Marcos Á. Medina <marcmedi1@hotmail.com>
2026-01-12 01:41:37 -0300

    refactor(MessageBubble): Remove unused translation import

--- apps/frontend/src/components/messaging/MessageBubble.tsx
+++ apps/frontend/src/components/messaging/MessageBubble.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { cn } from '@utils/cn';
 import type { ConversationHistoryResponse } from '@packages/contracts';
 
